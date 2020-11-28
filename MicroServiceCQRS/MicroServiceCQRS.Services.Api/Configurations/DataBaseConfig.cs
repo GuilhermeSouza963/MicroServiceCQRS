150dd6b5c6715a9e59ab9d97e849c533f40dcbca
guilhermepereira <guilhermepereira@uniaraxa.edu.br>
2020-11-27 21:40:29 -0300

    First Commit

---
+++ MicroServiceCQRS/MicroServiceCQRS.Services.Api/Configurations/DataBaseConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroServiceCQRS.Services.Api.Configurations
+{
+    public class DataBaseConfig
+    {
+    }
+}
